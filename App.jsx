28f88322c432b976724811a338f6bef51d1f2611
vyshnavi0 <86562120+vyshnavi0@users.noreply.github.com>
2021-06-27 20:15:50 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "/public/style.css";
+import "/public/index.html";
+import Info from "./Info";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Info(){
+  return(
+    <div className="note">
+      <h1>Java script and React.JS</h1>
+      <p> A basic web development React JS bootcamp by Shaurya sinha who is a data analyst</p>
+      </div>
+
+  );
+}
+export default Info;
